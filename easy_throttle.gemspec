fa54c97412b60b1bf76f6e480475b584ab204230
Damian Szalbierz <szalbierz.d.k@gmail.com>
2024-03-15 22:43:56 +0100

    Add EasyThrottle gem and Throttler class

---
+++ easy_throttle.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name        = 'EasyThrottle'
+  s.version     = '0.0.1'
+  s.licenses    = ['MIT']
+  s.summary     = "Allows you to throttle requests to an API."
+  s.authors     = ["Damian Szalbierz"]
+  s.email       = 'szalbierz.d.k@gmail.com'
+  s.files       = Dir["lib/**/*.rb"] + ["lib/config.yml"]
+  s.add_runtime_dependency 'redis', '>= 4.0'
+end
---
+++ lib/throttler.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require 'redis'
+
+class Throttler
+  DEFAULT_OPTIONS = { interval: 60, limit: 1, burst: 20 }.freeze
+  LIMITS = {
+  #   'endpointName' => { interval: 10, limit: 1, burst: 1 },
+  }.freeze
+
+  def self.with_throttling(endpoint, prefix, options = DEFAULT_OPTIONS, &block)
+    new(endpoint, prefix, options).with_throttling { block.call }
+  end
+
+  def initialize(endpoint, prefix, options = DEFAULT_OPTIONS)
+    @config = YAML.load_file('lib/config.yml')
+    @endpoint = endpoint
+    @prefix = prefix
+    @redis = Redis.new
+    @options = LIMITS.fetch(endpoint, options)
+  end
+
+  def with_throttling(&block)
+    retry_count ||= 0
+    result = nil
+    loop do
+      if pool
+        result = block.call
+
+        break
+      else
+        sleep 0.1
+      end
+    end
+    result
+  rescue *config['errors'].map(&:constantize) => e
+    if e.code == 429
+      extend_key_duration
+      retry
+    end
+
+    if e.code == 500
+      raise if (retry_count += 1) > 5
+      extend_key_duration
+      retry
+    end
+
+    raise
+  end
+
+  def pool
+    max_ttl = (options[:burst] - 1) * options[:interval] * 1000
+    burst_ttl = redis.pttl(burst_key)
+    burst_ttl = 0 if burst_ttl.negative?
+
+    if burst_ttl <= max_ttl
+      redis.psetex(burst_key, burst_ttl + options[:interval] * 1000, true)
+    end
+  end
+
+  private
+
+  attr_reader :redis, :endpoint, :prefix, :options
+
+  def burst_key
+    "#{prefix}:#{endpoint}:burst"
+  end
+
+  def extend_key_duration
+    redis.psetex(burst_key, (options[:interval] + 1) * options[:burst] * 1000, true)
+  end
+end
